c3d89cbf07bd38428bdeb8ac1fd71f72d2163c6f
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-18 17:59:25 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+sentence1 = "Hello there, and how are you?"
+sentence2 = "Hi again, just making sure it's reversed!"
+
+def reverse_each_word(string)
+  string = sentence1
+  array = sentence1.split("")
+  
+  array.each do |reverse|
+    
