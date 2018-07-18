d57300739500407cc3be3f29ff46ee18e88a1ac6
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-18 18:06:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ sentence1 = "Hello there, and how are you?"
 sentence2 = "Hi again, just making sure it's reversed!"
 
 def reverse_each_word(string)
-  array = []
   string = sentence1
   array = sentence1.split("")
   array << str.split(/,/)
