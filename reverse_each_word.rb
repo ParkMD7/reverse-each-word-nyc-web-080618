460724bdbb39c99f2d157ba935cfc64dfca56208
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-18 18:10:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ sentence2 = "Hi again, just making sure it's reversed!"
 
 def reverse_each_word(string)
   string = sentence1
-  sentence1 = sentence1.split("")
+  sentence1 = sentence1.split
    
   sentence1.collect do |word_reverse|
     word_reverse.reverse!
