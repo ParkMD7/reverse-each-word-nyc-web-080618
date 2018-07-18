9a1f8e3f20db38d77724d867ad1249d1b0f8fc59
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-18 18:10:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence1 = "Hello there, and how are you?"
 sentence2 = "Hi again, just making sure it's reversed!"
 
-def reverse_each_word(string)
+def reverse_each_word(sentence)
   string = sentence1
   sentence1 = sentence1.split
    
